8e3c998e38f7572ba0446d331f7e3b5f990fb9fd
dswook <leewook94@naver.com>
2024-12-21 14:37:59 +0900

    Revert "feat: add XGBoost ranker model"
    This reverts commit cd79d93a93abd3c7dd078c30a87db49f45a53a2d.

--- src/model/boosting.py
+++ src/model/boosting.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
 
-from typing import Self
-
 import lightgbm as lgb
 import numpy as np
 import pandas as pd
-import xgboost as xgb
 from omegaconf import DictConfig, OmegaConf
+from typing_extensions import Self
 
 from model.base import BaseModel
 
@@ -26,24 +24,11 @@ class LightGBMTrainer(BaseModel):
         params["seed"] = self.cfg.models.seed
         train_groups = X_train.groupby("reviewer_id").size().to_numpy()
         valid_groups = X_valid.groupby("reviewer_id").size().to_numpy()
+
         X_train, X_valid = X_train[self.cfg.data.features], X_valid[self.cfg.data.features]
 
-        train_set = lgb.Dataset(
-            X_train,
-            y_train,
-            params=params,
-            group=train_groups,
-            categorical_feature=[*self.cfg.data.cat_features],
-            feature_name=[*self.cfg.data.features],
-        )
-        valid_set = lgb.Dataset(
-            X_valid,
-            y_valid,
-            params=params,
-            group=valid_groups,
-            categorical_feature=[*self.cfg.data.cat_features],
-            feature_name=[*self.cfg.data.features],
-        )
+        train_set = lgb.Dataset(X_train, y_train, params=params, group=train_groups)
+        valid_set = lgb.Dataset(X_valid, y_valid, params=params, group=valid_groups)
 
         model = lgb.train(
             params=params,
@@ -54,41 +39,6 @@ class LightGBMTrainer(BaseModel):
                 lgb.log_evaluation(self.cfg.models.verbose_eval),
                 lgb.early_stopping(self.cfg.models.early_stopping_rounds),
             ],
-        )
-
-        return model
-
-
-class XGBoostTrainer(BaseModel):
-    def __init__(self, cfg: DictConfig) -> None:
-        super().__init__(cfg)
-
-    def _fit(
-        self: Self,
-        X_train: pd.DataFrame | np.ndarray,
-        y_train: pd.Series | np.ndarray,
-        X_valid: pd.DataFrame | np.ndarray | None = None,
-        y_valid: pd.Series | np.ndarray | None = None,
-    ) -> xgb.Booster:
-        params = OmegaConf.to_container(self.cfg.models.params)
-        params["seed"] = self.cfg.models.seed
-        train_groups = X_train.groupby("reviewer_id").size().to_numpy()
-        valid_groups = X_valid.groupby("reviewer_id").size().to_numpy()
-
-        X_train, X_valid = X_train[self.cfg.data.features], X_valid[self.cfg.data.features]
-
-        train_set = xgb.DMatrix(X_train, label=y_train)
-        train_set.set_group(train_groups)
-        valid_set = xgb.DMatrix(X_valid, label=y_valid)
-        valid_set.set_group(valid_groups)
-
-        model = xgb.train(
-            params=params,
-            dtrain=train_set,
-            num_boost_round=self.cfg.models.num_boost_round,
-            evals=[(train_set, "train"), (valid_set, "valid")],
-            early_stopping_rounds=self.cfg.models.early_stopping_rounds,
-            verbose_eval=self.cfg.models.verbose_eval,
         )
 
         return model
