24cbf2a95059d84680a8a97afd724595ef36da5c
ds-wook <leewook94@gmail.com>
2025-01-09 20:56:25 +0900

    test: update train_test_split_stratify_rank to use returned data structure and add additional assertions

--- tests/preprocess/test_preprocess.py
+++ tests/preprocess/test_preprocess.py
@@ -31,7 +31,7 @@ def test_train_test_split_stratify():
 
 
 def test_train_test_split_stratify_rank():
-    X_train, y_train, X_val, y_val = train_test_split_stratify(
+    data = train_test_split_stratify(
         test_size=0.2,
         min_reviews=10,
         random_state=42,
@@ -39,9 +39,13 @@ def test_train_test_split_stratify_rank():
         is_rank=True,
     )
 
-    assert len(X_train) > 10000
-    assert len(X_val) > 10000
-    assert len(y_train) > 10000
-    assert len(y_val) > 10000
-    assert len(X_train) == len(y_train)
-    assert len(X_val) == len(y_val)
+    assert data["X_train"].shape[0] > 10000
+    assert data["X_val"].shape[0] > 10000
+    assert data["y_train"].shape[0] > 10000
+    assert data["y_val"].shape[0] > 10000
+    assert data["X_train"].shape[0] == data["y_train"].shape[0]
+    assert data["X_val"].shape[0] == data["y_val"].shape[0]
+    assert data["num_diners"] > 0
+    assert data["num_users"] > 0
+    assert data["diner_mapping"] is not None
+    assert data["user_mapping"] is not None
