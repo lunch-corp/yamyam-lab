3d884901d9322cd473bc7b9cc09f4b97d6774962
bohyun-shin <bohyun.shin@linecorp.com>
2025-05-31 16:17:44 +0900

    feat: add functionality in `prepare_train_val_dataset` method in data loader  - IMPORTANT: fix bug incorrectly defining test dataset using validation dataset  - add `is_csr` argument for models using csr dataset such as als  - pass diner_ids in each of train/val/test to calculate density in each dataset

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import torch
 from numpy.typing import NDArray
+from scipy.sparse import csr_matrix
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
@@ -305,13 +306,16 @@ class DatasetLoader:
         self: Self,
         is_rank: bool = False,
         use_metadata: bool = False,
+        is_csr: bool = False,
     ) -> Dict[str, Any]:
         """
         Load and process training data.
 
         Args:
-            is_rank: bool
-            use_metadata: bool
+            is_rank (bool): Indicator if it is ranking model or not.
+            use_metadata (bool): Indicator if using metadata or not in graph model.
+            is_candidate_dataset (bool): Indicator if using candidate dataset or not.
+            is_csr (bool): Indicator if csr format or not for als model.
 
         Returns (Dict[str, Any]):
             A dictionary containing the training and validation sets.
@@ -497,10 +501,39 @@ class DatasetLoader:
         train_user_ids = train["reviewer_id"].unique()
         val_user_ids = val["reviewer_id"].unique()
         test_user_ids = test["reviewer_id"].unique()
+        train_diner_ids = train["diner_idx"].unique()
+        val_diner_ids = val["diner_idx"].unique()
+        test_diner_ids = test["diner_idx"].unique()
         val_warm_users = val[lambda x: x["reviewer_id"].isin(val_warm_start_user_ids)]
         val_cold_users = val[lambda x: x["reviewer_id"].isin(val_cold_start_user_ids)]
-        test_warm_users = val[lambda x: x["reviewer_id"].isin(test_warm_start_user_ids)]
-        test_cold_users = val[lambda x: x["reviewer_id"].isin(test_cold_start_user_ids)]
+        test_warm_users = test[
+            lambda x: x["reviewer_id"].isin(test_warm_start_user_ids)
+        ]
+        test_cold_users = test[
+            lambda x: x["reviewer_id"].isin(test_cold_start_user_ids)
+        ]
+
+        if is_csr:
+            return self.create_csr_dataset(
+                train=train,
+                val=val,
+                test=test,
+                train_user_ids=train_user_ids,
+                val_user_ids=val_user_ids,
+                test_user_ids=test_user_ids,
+                train_diner_ids=train_diner_ids,
+                val_diner_ids=val_diner_ids,
+                test_diner_ids=test_diner_ids,
+                val_warm_start_user_ids=val_warm_start_user_ids,
+                val_cold_start_user_ids=val_cold_start_user_ids,
+                test_warm_start_user_ids=test_warm_start_user_ids,
+                test_cold_start_user_ids=test_cold_start_user_ids,
+                val_warm_users=val_warm_users,
+                val_cold_users=val_cold_users,
+                test_warm_users=test_warm_users,
+                test_cold_users=test_cold_users,
+                mapped_res=mapped_res,
+            )
 
         if use_metadata:
             meta_mapping_info = meta_mapping(
@@ -517,6 +550,9 @@ class DatasetLoader:
             train_user_ids=train_user_ids,
             val_user_ids=val_user_ids,
             test_user_ids=test_user_ids,
+            train_diner_ids=train_diner_ids,
+            val_diner_ids=val_diner_ids,
+            test_diner_ids=test_diner_ids,
             val_warm_start_user_ids=val_warm_start_user_ids,
             val_cold_start_user_ids=val_cold_start_user_ids,
             test_warm_start_user_ids=test_warm_start_user_ids,
@@ -716,6 +752,73 @@ class DatasetLoader:
             **mapped_res,
         }
 
+    def create_csr_dataset(
+        self: Self,
+        train: pd.DataFrame,
+        val: pd.DataFrame,
+        test: pd.DataFrame,
+        train_user_ids: NDArray,
+        val_user_ids: NDArray,
+        test_user_ids: NDArray,
+        train_diner_ids: NDArray,
+        val_diner_ids: NDArray,
+        test_diner_ids: NDArray,
+        val_warm_start_user_ids: NDArray,
+        val_cold_start_user_ids: NDArray,
+        test_warm_start_user_ids: NDArray,
+        test_cold_start_user_ids: NDArray,
+        val_warm_users: pd.DataFrame,
+        val_cold_users: pd.DataFrame,
+        test_warm_users: pd.DataFrame,
+        test_cold_users: pd.DataFrame,
+        mapped_res: Dict[str, Any],
+    ) -> Dict[str, Any]:
+        num_total_users = len(mapped_res.get("user_mapping"))
+        num_total_diners = len(mapped_res.get("diner_mapping"))
+        train_Cui_csr = self.create_csr_matrix_from_review_data(
+            df=train,
+            num_total_users=num_total_users,
+            num_total_diners=num_total_diners,
+        )
+        val_Cui_csr = self.create_csr_matrix_from_review_data(
+            df=val,
+            num_total_users=num_total_users,
+            num_total_diners=num_total_diners,
+        )
+        test_Cui_csr = self.create_csr_matrix_from_review_data(
+            df=test,
+            num_total_users=num_total_users,
+            num_total_diners=num_total_diners,
+        )
+        return {
+            "X_train": train_Cui_csr,
+            "X_val": val_Cui_csr,
+            "X_test": test_Cui_csr,
+            "X_train_df": train,
+            "X_val_warm_users": val_warm_users[self.X_columns],
+            "y_val_warm_users": val_warm_users[self.y_columns],
+            "X_val_cold_users": val_cold_users[self.X_columns],
+            "y_val_cold_users": val_cold_users[self.y_columns],
+            "X_test_warm_users": test_warm_users[self.X_columns],
+            "y_test_warm_users": test_warm_users[self.y_columns],
+            "X_test_cold_users": test_cold_users[self.X_columns],
+            "y_test_cold_users": test_cold_users[self.y_columns],
+            "most_popular_diner_ids": self.get_most_popular_diner_ids(
+                train_review=train
+            ),
+            "val_warm_start_user_ids": val_warm_start_user_ids,
+            "val_cold_start_user_ids": val_cold_start_user_ids,
+            "test_warm_start_user_ids": test_warm_start_user_ids,
+            "test_cold_start_user_ids": test_cold_start_user_ids,
+            "train_user_ids": train_user_ids,
+            "val_user_ids": val_user_ids,
+            "test_user_ids": test_user_ids,
+            "train_diner_ids": train_diner_ids,
+            "val_diner_ids": val_diner_ids,
+            "test_diner_ids": test_diner_ids,
+            **mapped_res,
+        }
+
     def load_candidate_dataset(
         self: Self,
         user_feature: pd.DataFrame,
@@ -819,6 +922,9 @@ class DatasetLoader:
         train_user_ids: NDArray,
         val_user_ids: NDArray,
         test_user_ids: NDArray,
+        train_diner_ids: NDArray,
+        val_diner_ids: NDArray,
+        test_diner_ids: NDArray,
         val_warm_start_user_ids: NDArray,
         val_cold_start_user_ids: NDArray,
         test_warm_start_user_ids: NDArray,
@@ -892,6 +998,9 @@ class DatasetLoader:
             "train_user_ids": train_user_ids,
             "val_user_ids": val_user_ids,
             "test_user_ids": test_user_ids,
+            "train_diner_ids": train_diner_ids,
+            "val_diner_ids": val_diner_ids,
+            "test_diner_ids": test_diner_ids,
             **mapped_res,
         }
 
@@ -928,6 +1037,21 @@ class DatasetLoader:
         warm_start_user_ids = np.array(list(train_user_ids & test_user_ids))
         cold_start_user_ids = np.array(list(test_user_ids - train_user_ids))
         return warm_start_user_ids, cold_start_user_ids
+
+    def create_csr_matrix_from_review_data(
+        self: Self,
+        df: pd.DataFrame,
+        num_total_users: int,
+        num_total_diners: int,
+    ) -> csr_matrix:
+        """
+        Create csr matrix from review data for als model.
+        """
+        Cui_csr = csr_matrix(
+            (df["reviewer_review_score"], (df["reviewer_id"], df["diner_idx"])),
+            shape=(num_total_users, num_total_diners),
+        )
+        return Cui_csr
 
     @staticmethod
     def is_valid_date_format(date_string: str) -> bool:
