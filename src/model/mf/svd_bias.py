028019dbf0f593cf35313f2cbde8f58b0bdeff03
bohyun-shin <bohyun.shin@linecorp.com>
2025-06-07 17:34:47 +0900

    refactor: remove unnecessary parent class, substitue to torch nn.Module in svd_bias model

--- src/model/mf/svd_bias.py
+++ src/model/mf/svd_bias.py
@@ -1,16 +1,13 @@
-from typing import Dict, List
+from typing import List
 
-import numpy as np
-import pandas as pd
 import torch
 import torch.nn as nn
 from torch import Tensor
 
 from constant.metric.metric import Metric
-from model.graph.base_embedding import BaseEmbedding
 
 
-class Model(BaseEmbedding):
+class Model(nn.Module):
     def __init__(
         self,
         user_ids: torch.Tensor,
@@ -25,12 +22,12 @@ class Model(BaseEmbedding):
             num_items (int): number of unique items (diners) across train / validation dataset.
             embedding_dim (int): dimension size of embedding vector.
         """
-        super().__init__(
-            user_ids=user_ids,
-            diner_ids=diner_ids,
-            top_k_values=top_k_values,
-            embedding_dim=embedding_dim,
-        )
+        super().__init__()
+        self.user_ids = user_ids
+        self.diner_ids = diner_ids
+        self.top_k_values = top_k_values
+        self.embedding_dim = embedding_dim
+
         num_users = user_ids.size(0)
         num_items = diner_ids.size(0)
 
@@ -85,92 +82,3 @@ class Model(BaseEmbedding):
             + self.mu
         )  # batch_size * 1
         return output
-
-    def _generate_recommendations_and_calculate_metric_for_warm_start_users(
-        self,
-        X_train: Tensor,
-        X_val_warm_users: Tensor,
-        top_k_values: List[int],
-        metric_at_k: Dict,
-        filter_already_liked: bool = True,
-    ) -> Dict:
-        """
-        Overwrite `_generate_recommendations_and_calculate_metric_for_warm_start_users` method in BaseEmbedding.
-
-        Args:
-             X_train (Tensor): number of reviews x (diner_id, reviewer_id) in train dataset.
-             X_val (Tensor): number of reviews x (diner_id, reviewer_id) in val dataset.
-             top_k_values (List[int]): a list of k values.
-             filter_already_liked (bool): whether filtering pre-liked diner in train dataset or not.
-        """
-        max_k = max(top_k_values)
-        start = 0
-        diner_embeds = self.embed_item(self.diner_ids)
-
-        self.train_liked_series = (
-            pd.DataFrame(X_train, columns=["diner_idx", "reviewer_id"])
-            .groupby("reviewer_id")["diner_idx"]
-            .apply(np.array)
-        )
-        self.train_liked = (
-            self.train_liked_series.to_frame()
-            .reset_index()
-            .assign(num_liked_items=lambda df: df["diner_idx"].apply(len))
-        )
-        self.val_liked_series = (
-            pd.DataFrame(X_val_warm_users, columns=["diner_idx", "reviewer_id"])
-            .groupby("reviewer_id")["diner_idx"]
-            .apply(np.array)
-        )
-        self.val_liked = (
-            self.val_liked_series.to_frame()
-            .reset_index()
-            .assign(num_liked_items=lambda df: df["diner_idx"].apply(len))
-            .sort_values(by="reviewer_id")
-        )
-
-        liked_items_count2user_ids = (
-            self.val_liked.groupby("num_liked_items")["reviewer_id"]
-            .apply(np.array)
-            .to_dict()
-        )
-
-        for count, user_ids in liked_items_count2user_ids.items():
-            num_users = len(user_ids)
-            start = 0
-            user_ids = torch.tensor(user_ids, device=self.device)
-
-            while start < num_users:
-                batch_users = user_ids[start : start + self.recommend_batch_size]
-                user_embeds = self.embed_user(batch_users)
-                scores = torch.mm(user_embeds, diner_embeds.t())
-                liked_items_by_batch_users = []
-
-                # TODO: change for loop to more efficient program
-                # filter diner id already liked by user in train dataset
-                if filter_already_liked:
-                    for i, user_id in enumerate(batch_users):
-                        already_liked_ids = self.train_liked_series[user_id.item()]
-                        scores[i][already_liked_ids] = -float("inf")
-
-                batch_users_np = batch_users.detach().cpu().numpy()
-                liked_items_by_batch_users = np.vstack(
-                    self.val_liked[lambda x: x["reviewer_id"].isin(batch_users_np)][
-                        "diner_idx"
-                    ].values
-                )
-
-                max_k = min(scores.shape[1], max_k)  # to prevent index error in pytest
-                top_k = torch.topk(scores, k=max_k)
-                top_k_id = top_k.indices
-
-                self.calculate_metric_at_current_batch(
-                    metric_at_k=metric_at_k,
-                    top_k_id=top_k_id.detach().cpu().numpy(),
-                    liked_items=liked_items_by_batch_users,
-                    top_k_values=top_k_values,
-                )
-
-                start += self.recommend_batch_size
-
-        return metric_at_k
