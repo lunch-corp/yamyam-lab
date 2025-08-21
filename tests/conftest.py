4c8eee578bcd2c89062e3d62f882e774e824fb3f
ds-wook <leewook94@gmail.com>
2025-08-21 12:27:44 +0900

    fix: update test configuration to disable test mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,7 +24,7 @@ def setup_data_config(request):
                 "diner_review_tags": {},
                 "diner_menu_price": {},
             },
-            "test": True,
+            "test": False,
             "random_state": 42,
             "stratify": "reviewer_id",
             "use_unique_mapping_id": False,
