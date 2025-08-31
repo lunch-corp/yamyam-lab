737771d2d2f85016fc8561eefef50d96788e1339
bohyun-shin <bohyun.shin@linecorp.com>
2025-08-31 15:29:44 +0900

    feat: use pre-defined diner_ids with abusive reviews to avoid redundant computation

--- src/preprocess/filter.py
+++ src/preprocess/filter.py
@@ -28,6 +28,7 @@ class Filter:
         min_review_count_by_diner_id: int,
         included_tags: List[str],
         abusive_words: List[str],
+        pre_calculated_diner_ids: List[int],
     ) -> pd.DataFrame:
         """
         Filter abusive reviews talking about martial laws when 2024-12 ~ 2025-01.
@@ -39,6 +40,7 @@ class Filter:
             min_review_count_by_diner_id (int): Minimum review count which has abusive words.
             included_tags (List[str]): Morpheme tags to be included.
             abusive_words (List[str]): Pre-defined abusive words related with martial law.
+            pre_calculated_diner_ids (List[int]): Pre-calculated diner_ids with abusive reviews to skip calculation.
 
         Returns (pd.DataFrame):
             Review after excluding abusive reviews with martial law.
@@ -53,43 +55,49 @@ class Filter:
             .map(lambda x: x[:7])
             .isin(target_months)
         ]
-        # tokenize review and count them
-        start = time.time()
-        review_filtered["token_count"] = review_filtered["reviewer_review"].map(
-            lambda x: Counter(tokenize_with_kiwi(self.kiwi, x, included_tags))
-        )
-        # Note: this code line takes about 220 seconds.
-        self.logger.info(f"Token time for tokenizing: {round(time.time() - start, 2)}")
+        if len(pre_calculated_diner_ids) == 0:
+            # tokenize review and count them
+            start = time.time()
+            review_filtered["token_count"] = review_filtered["reviewer_review"].map(
+                lambda x: Counter(tokenize_with_kiwi(self.kiwi, x, included_tags))
+            )
+            # Note: this code line takes about 220 seconds.
+            self.logger.info(
+                f"Token time for tokenizing: {round(time.time() - start, 2)}"
+            )
 
-        # tag if each review is talking about martial law or not by overlapping word
-        review_filtered["is_martial_law_review"] = review_filtered["token_count"].map(
-            lambda x: len(set(x.keys()) & set(abusive_words)) >= 1
-        )
-        # sum martial law review counts by diner_id
-        martial_law_review_count_by_diner_id = (
-            review_filtered.groupby("diner_idx")["is_martial_law_review"]
-            .sum()
-            .to_dict()
-        )
+            # tag if each review is talking about martial law or not by overlapping word
+            review_filtered["is_martial_law_review"] = review_filtered[
+                "token_count"
+            ].map(lambda x: len(set(x.keys()) & set(abusive_words)) >= 1)
+            # sum martial law review counts by diner_id
+            martial_law_review_count_by_diner_id = (
+                review_filtered.groupby("diner_idx")["is_martial_law_review"]
+                .sum()
+                .to_dict()
+            )
 
-        # sum token counts by diner_id
-        token_count_by_diner_id = (
-            review_filtered.groupby("diner_idx")["token_count"].sum().to_dict()
-        )
+            # sum token counts by diner_id
+            token_count_by_diner_id = (
+                review_filtered.groupby("diner_idx")["token_count"].sum().to_dict()
+            )
 
-        # apply abusing detection rule
-        # [Rule]
-        # 1. after aggregation token count by diner_id, if there are more or equal min_common_word_count_with_abusive_words words
-        # 2. if there are more or equal min_review_count_by_diner_id reviews containing martial law tokens
-        diner_ids_with_martial_law_reviews = []
-        for diner_id, token_count in token_count_by_diner_id.items():
-            if (
-                len(set(token_count.keys()) & set(abusive_words))
-                >= min_common_word_count_with_abusive_words
-                and martial_law_review_count_by_diner_id.get(diner_id, 0)
-                >= min_review_count_by_diner_id
-            ):
-                diner_ids_with_martial_law_reviews.append(diner_id)
+            # apply abusing detection rule
+            # [Rule]
+            # 1. after aggregation token count by diner_id, if there are more or equal min_common_word_count_with_abusive_words words
+            # 2. if there are more or equal min_review_count_by_diner_id reviews containing martial law tokens
+            diner_ids_with_martial_law_reviews = []
+            for diner_id, token_count in token_count_by_diner_id.items():
+                if (
+                    len(set(token_count.keys()) & set(abusive_words))
+                    >= min_common_word_count_with_abusive_words
+                    and martial_law_review_count_by_diner_id.get(diner_id, 0)
+                    >= min_review_count_by_diner_id
+                ):
+                    diner_ids_with_martial_law_reviews.append(diner_id)
+        else:
+            diner_ids_with_martial_law_reviews = pre_calculated_diner_ids[:]
+            self.logger.info("Using pre-calculated diner_ids with abusive reviews")
         self.logger.info(
             f"Detected {len(diner_ids_with_martial_law_reviews)} diner_ids with abusive reviews: {diner_ids_with_martial_law_reviews}"
         )
