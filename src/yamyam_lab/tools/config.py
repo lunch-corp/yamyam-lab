10627df6f071f9d3d699f6e1941978219672405c
bohyun-shin <bohyun.shin@linecorp.com>
2025-10-26 10:21:16 +0900

    feat: add util functions

--- src/yamyam_lab/tools/config.py
+++ src/yamyam_lab/tools/config.py
@@ -1,3 +1,7 @@
+import os
+from datetime import datetime
+from typing import Tuple
+
 import yaml
 from easydict import EasyDict
 
@@ -6,3 +10,35 @@ def load_yaml(path: str):
     with open(path, "r") as file:
         config = yaml.safe_load(file)
     return EasyDict(config)
+
+
+def load_configs(model: str, config_root_path: str = None) -> Tuple[EasyDict, EasyDict]:
+    if model in ["node2vec", "metapath2vec", "graphsage", "lightgcn"]:
+        model_type = "graph"
+    elif model in ["svd_bias", "als"]:
+        model_type = "mf"
+    else:
+        raise ValueError(f"Unsupported model type: {model}")
+    if config_root_path is None:
+        config_root_path = os.path.join(os.path.dirname(__file__), "../../../config")
+
+    model_config = load_yaml(
+        os.path.join(config_root_path, f"./models/{model_type}/{model}.yaml")
+    )
+    preprocess_config = load_yaml(
+        os.path.join(config_root_path, "./preprocess/preprocess.yaml")
+    )
+
+    return model_config, preprocess_config
+
+
+def generate_result_path(model: str, test: bool, result_path: str = None) -> str:
+    if result_path is not None:
+        return result_path
+
+    root_path = os.path.join(os.path.dirname(__file__), "../../..")
+    dt = datetime.now().strftime("%Y%m%d%H%M%S")
+    test_flag = "test" if test else "untest"
+    result_path = os.path.join(root_path, f"./result/{test_flag}/{model}/{dt}")
+    os.makedirs(result_path, exist_ok=True)
+    return result_path
