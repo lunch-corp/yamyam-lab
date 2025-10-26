c63b315d7f625fb942d67bb32d4b790be90d91de
ds-wook <leewook94@gmail.com>
2025-10-26 17:15:02 +0900

    refactor: simplify __all__ declaration in engine module

--- src/yamyam_lab/engine/__init__.py
+++ src/yamyam_lab/engine/__init__.py
@@ -3,7 +3,4 @@
 from yamyam_lab.engine.base_trainer import BaseTrainer
 from yamyam_lab.engine.factory import TrainerFactory
 
-__all__ = [
-    "BaseTrainer",
-    "TrainerFactory",
-]
+__all__ = ["BaseTrainer", "TrainerFactory"]
